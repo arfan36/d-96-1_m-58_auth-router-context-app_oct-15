15a6afe805428c19529e392baf4d88e381f5e0b2
arfan36 <arfanur36@gmail.com>
2022-10-18 15:34:44 +0600

    header added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div>
-            <h2>header</h2>
+            <div className="navbar bg-primary text-primary-content">
+                <Link className="btn btn-ghost normal-case text-xl" to={'/'}>Awesome Auth</Link>
+                <Link className="btn btn-ghost normal-case text-xl" to={'/'}>Home</Link>
+                <Link className="btn btn-ghost normal-case text-xl" to={'/login'}>Log in</Link>
+                <Link className="btn btn-ghost normal-case text-xl" to={'/register'}>Register</Link>
+            </div>
         </div>
     );
 };
