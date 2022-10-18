c1e354f311b880715633875fa8c767c0084cf560
arfan36 <arfanur36@gmail.com>
2022-10-18 17:10:07 +0600

    auth variable added

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,7 +1,10 @@
 import React, { createContext, useState } from 'react';
+import { getAuth } from "firebase/auth";
+import app from '../firebase/firebase.config';
 
 export const AuthContext = createContext();
 
+const auth = getAuth(app);
 
 const UserContext = ({ children }) => {
     const [user, setUser] = useState({ displayName: 'Aakash' });
